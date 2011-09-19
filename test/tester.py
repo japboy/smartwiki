cc44d37dc916e96befb2faeee5c63d46b48b614e
Yu I <lagfer@youck.org>
2011-09-19 16:31:20 +0900

    Update test driver script tester.py

--- test/tester.py
+++ test/tester.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Test suite & runner module
+u"""Test suite & runner module
 
-This is the wrapper module of all the tests. This tester depends on Google App
-Engine SDK.
+This is the wrapper module of all the tests for Google App Engine SDK. This will
+automatically read `test_*.py` in the same directory and run the tests.
 """
 
 import os
 import sys
 import unittest
 
-__author__ = 'Yu I.'
-__copyright__ = 'Copyright 2011, Yu I.'
-__license__ = 'Public domain'
-__version__ = '0.1'
+__author__     = u'Yu I.'
+__credits__    = [u'None']
+__license__    = u'Public domain'
+__version__    = u'0.2'
 
-SDK_PATH = '/path/to/google_appengine-1.5.4'
-APP_PATH = '/path/to/com.appspot.driwak/src'
+SDK_PATH = os.path.join(os.path.expanduser(u'~'),
+                        u'Applications/google_appengine-1.5.4')
+APP_PATH = os.path.join(os.path.expanduser(u'~'),
+                        u'Workspace/com.appspot.driwak/src')
 TEST_PATH = os.path.dirname(__file__)
 
 def main(sdk_path=SDK_PATH, app_path=APP_PATH, test_path=TEST_PATH):
